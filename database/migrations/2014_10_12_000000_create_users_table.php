add48c060b308290ac4e2307e6d741c6ff3bf9a6
radon83 <rodainahab@gmail.com>
2022-06-15 20:27:05 +0300

    add migrations

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,8 +19,8 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('img');
-            $table->date('date_of_birth');
+            $table->string('img')->nullable();
+            $table->date('date_of_birth')->nullable();
             $table->enum('role', ['admin', 'user'])->default('user');
             $table->enum('status', ['active', 'inactive'])->default('active');
             $table->rememberToken();
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UserSeeder::class);
+        //factory(App\User::class, 10)->create(); // create 10 users
+        factory(App\Post::class, 10)->create(); // create 10 posts
     }
 }
